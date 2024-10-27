ba36f7f7c9f3e86eb7e9204eacbe8a8c86c7317f
Fukase-Takamasa <ultrafukase@gmail.com>
2024-10-27 12:24:34 +0900

    Main.storyboardなど不要なファイルを削除し、TabBarControllerが初期状態で表示されるように調整

--- YoutubeViewer/SceneDelegate.swift
+++ YoutubeViewer/SceneDelegate.swift
@@ -11,11 +11,11 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
 
-
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        guard let _ = (scene as? UIWindowScene) else { return }
-        let tabBarController = TabBarController()
-        window?.rootViewController = tabBarController
+        guard let windowScene = (scene as? UIWindowScene) else { return }
+        window = UIWindow(windowScene: windowScene)
+        window?.rootViewController = TabBarController()
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {

0d60e480ec93559a6c09c4e604e1b2c10a94b03d
Fukase-Takamasa <ultrafukase@gmail.com>
2024-10-28 14:40:10 +0900

    PanModalで色々試したやつ。最初はフルスクリーンで表示させて、下に下ろしたら小さい状態でハーフモーダルで止まる実装など。（ハーフモーダル状態からは閉じれなくできたけど、フル状態から一気に一番下まで下ろすと閉じれてしまう）

--- YoutubeViewer/VideoDetail/VideoDetailViewController.swift
+++ YoutubeViewer/VideoDetail/VideoDetailViewController.swift
@@ -7,9 +7,12 @@
 
 import UIKit
 import Kingfisher
+import PanModal
 
 class VideoDetailViewController: UIViewController {
-
+    var isViewAppeared = false
+    var isLongForm = false
+    
     @IBOutlet weak var videoImageView: UIImageView!
     @IBOutlet weak var descriptionLabel: UILabel!
     
@@ -17,5 +20,70 @@ class VideoDetailViewController: UIViewController {
         super.viewDidLoad()
         
         videoImageView.kf.setImage(with: URL(string: "https://www.tabemaro.jp/wp-content/uploads/2023/06/27910319_m-1700x1133.jpg"))
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        isViewAppeared = true
+        panModalSetNeedsLayoutUpdate()
+    }
+}
+
+extension VideoDetailViewController: PanModalPresentable {
+    var panScrollable: UIScrollView? {
+        nil
+    }
+    
+    var topOffset: CGFloat {
+        return 0.0
+    }
+
+    var springDamping: CGFloat {
+        return 1.0
+    }
+
+    var transitionDuration: Double {
+        return 0.4
+    }
+
+    var transitionAnimationOptions: UIView.AnimationOptions {
+        return [.allowUserInteraction, .beginFromCurrentState]
+    }
+
+    var shouldRoundTopCorners: Bool {
+        return false
+    }
+
+    var showDragIndicator: Bool {
+        return false
+    }
+    
+    var longFormHeight: PanModalHeight {
+        return .maxHeight
+    }
+    
+    var shortFormHeight: PanModalHeight {
+        if isViewAppeared {
+            return .contentHeight(200)
+        }else {
+            return .maxHeight
+        }
+    }
+    
+    var allowsTapToDismiss: Bool {
+        return false
+    }
+    
+    var allowsDragToDismiss: Bool {
+        return !isLongForm
+    }
+    
+    func willTransition(to state: PanModalPresentationController.PresentationState) {
+        if state == .longForm {
+            isLongForm = true
+        }else {
+            isLongForm = false
+        }
     }
 }

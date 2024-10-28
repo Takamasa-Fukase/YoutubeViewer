5a45adb9097355841eab65d970329f284b1b6f39
Fukase-Takamasa <ultrafukase@gmail.com>
2024-10-28 21:49:27 +0900

    FloatingImageVCのImageViewを、outletを使わずにコードで生成するように差し替え（storyboard上で制約を設定した関係でframeの変更が反映されないという干渉が起きていたため）

--- YoutubeViewer/FloatingImage/FloatingImageViewController.swift
+++ YoutubeViewer/FloatingImage/FloatingImageViewController.swift
@@ -9,56 +9,45 @@ import UIKit
 import Kingfisher
 
 class FloatingImageViewController: UIViewController {
-    
-    @IBOutlet weak var videoImageView: UIImageView!
-    @IBOutlet weak var dummyImageView: UIImageView!
+    var videoImageView: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        videoImageView.kf.setImage(with: URL(string: "https://www.tabemaro.jp/wp-content/uploads/2023/06/27910319_m-1700x1133.jpg"))
-        videoImageView.layer.cornerRadius = 6
-        dummyImageView.kf.setImage(with: URL(string: "https://www.tabemaro.jp/wp-content/uploads/2023/06/27910319_m-1700x1133.jpg"))
-        dummyImageView.layer.cornerRadius = 6
+        
+        setupImageView()
     }
     
-//    override func viewDidLayoutSubviews() {
-//        super.viewDidLayoutSubviews()
-//
-//        print("videoImageViewのframe: \(videoImageView.frame)") // (0.0, 59.0, 393.0, 221.0)
-//        print("dummyImageViewのframe: \(dummyImageView.frame)") // (184.66666666666663, 653.0, 200.33333333333334, 107.0)
-//    }
-    
+    private func setupImageView() {
+        guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
+        videoImageView = UIImageView(
+            frame: CGRect(
+                x: 0,
+                y: windowScene.keyWindow?.safeAreaInsets.top ?? 0,
+                width: view.frame.width,
+                height: view.frame.width * 0.5625
+            )
+        )
+        videoImageView.contentMode = .scaleAspectFill
+        videoImageView.clipsToBounds = true
+        videoImageView.layer.cornerRadius = 6
+        videoImageView.kf.setImage(with: URL(string: "https://www.tabemaro.jp/wp-content/uploads/2023/06/27910319_m-1700x1133.jpg"))
+        view.addSubview(videoImageView)
+    }
+
     func updateImageViewFrame(dismissalProgress: Float, tabBarHeight: CGFloat) {
-        // まずTabBarの高さを取得とそれに加える余白を元に、maxYを決める（screenHeight - TabBarHeight + 余白）
-//        let tabBarHeight = tabBarController?.tabBar.frame.height ?? 0.0
-        print("tabBarHeight: \(tabBarHeight)")
         guard let windowScene = UIApplication.shared.connectedScenes.first as? UIWindowScene else { return }
         let newWidth = windowScene.screen.bounds.width * 0.51
-        // 横幅が決まれば高さは16:9の割合の計算で求める(* 0.5625)
+        // 高さは16:9の割合の計算で求める(width * 0.5625)
         let newHeight = newWidth * 0.5625
-        let maxX = windowScene.screen.bounds.width - 8
-        //  また、端末の横幅（screenWidth）から0.51倍したやつを横幅にし、右端に余白分をつけた状態でのmaxXを決める
-        let minX = windowScene.screen.bounds.width - (newWidth) - 8
-        // TabBarの高さと余白分を差し引く
-        let maxY = windowScene.screen.bounds.height - tabBarHeight - 4
-        let minY = maxY - newHeight
-        // minY（frameのYに入れる値）はmaxY - ImageViewの高さ
-        // minX（frameのXに入れる値）はmaxX - ImageViewの横幅
-        
-        // centerY = minY + (newHeight / 2)
-        // centerX = minX + (newWidth / 2)
+        // 画面の横幅から(余白, ImageViewの高さ)を差し引く
+        let minX = windowScene.screen.bounds.width - 8 - newWidth
+        // 画面の高さから(TabBarの高さ, 余白, ImageViewの高さ)を差し引く
+        let minY = windowScene.screen.bounds.height - tabBarHeight - 4 - newHeight
         
         let newFrame = CGRect(x: minX,           //   0.0 ~ 184.6
                               y: minY,           //  59.0 ~ 653.0
                               width: newWidth,   // 393.0 ~ 200.3
                               height: newHeight) // 221.0 ~ 107.0
-        
-        videoImageView.center.x = maxX - (newWidth / 2)
-        videoImageView.center.y = maxY - (newHeight / 2)
-        videoImageView.contentMode = .redraw
-//        videoImageView.bounds.size = CGSize(width: newWidth, height: newHeight)
-//        videoImageView.bounds = newFrame
-        videoImageView.contentMode = .scaleAspectFill
+        videoImageView.frame = newFrame
     }
 }

517255941efca9afab7a3bacf0cc07917e6d168d
Fukase-Takamasa <ultrafukase@gmail.com>
2024-10-30 23:20:56 +0900

    命名修正など

--- YoutubeViewer/VideoDetail/VideoDetailViewController.swift
+++ YoutubeViewer/VideoDetail/VideoDetailViewController.swift
@@ -22,24 +22,23 @@ class VideoDetailViewController: UIViewController {
             switch screenMode {
             case .fullScreen:
                 playerBaseView.layer.cornerRadius = 0
-                imageViewCloseButton.isHidden = true
+                playerCloseButton.isHidden = true
             case .changing:
                 playerBaseView.layer.cornerRadius = 10
-                imageViewCloseButton.isHidden = true
+                playerCloseButton.isHidden = true
             case .small:
                 playerBaseView.layer.cornerRadius = 10
-                imageViewCloseButton.isHidden = false
+                playerCloseButton.isHidden = false
             }
         }
     }
     
     private var playerView: WKYTPlayerView!
-    private var imageViewCloseButton: UIButton!
+    private var playerCloseButton: UIButton!
     private var descriptionBaseView: UIView!
-    private var descriptionView: VideoDetailDescriptionView!
     private var backgroundMaskingView: UIView!
     private var initialDragPositionY: CGFloat = 0.0
-    private var initialImageViewFrame: CGRect!
+    private var initialPlayerBaseViewFrame: CGRect!
     private var isShownModalPresentationAnimation = false
     
     // 0.0 ~ 1.0の範囲
@@ -47,13 +46,13 @@ class VideoDetailViewController: UIViewController {
     //  - 1.0: 最小化された状態
     private var minimizationProgress: CGFloat = 0.0 {
         didSet {
-            // ImageViewのサイズと座標を更新
-            updateImageViewSizeAndPosition(minimizationProgress: minimizationProgress)
+            // playerBaseViewのサイズと座標を更新
+            updatePlayerBaseViewSizeAndPosition(minimizationProgress: minimizationProgress)
             
             // description部分のViewの透明度を更新（最小化の進行度合いの0.0 ~ 0.3の範囲を1.0 ~ 0.0の割合に変換）
             descriptionBaseView.alpha = 1.0 - (minimizationProgress * 3.33)
             
-            // description部分のViewのY座標をImageViewの下端に合わせて更新
+            // description部分のViewのY座標をplayerBaseViewの下端に合わせて更新
             descriptionBaseView.frame.origin.y = playerBaseView.frame.maxY
 
             // 背面を暗くしているViewの透明度を更新（最小化の進行度合いの0.0 ~ 0.3の範囲を0.5 ~ 0.0の割合に変換）
@@ -87,16 +86,16 @@ class VideoDetailViewController: UIViewController {
     }
     
     private func setupPlayerView() {
-        initialImageViewFrame = CGRect(
+        initialPlayerBaseViewFrame = CGRect(
             x: 0,
             y: SceneDelegate.shared?.mainWindow?.safeAreaInsets.top ?? 0,
             width: view.frame.width,
             height: view.frame.width * 0.5625
         )
-        playerBaseView = UIView(frame: initialImageViewFrame)
+        playerBaseView = UIView(frame: initialPlayerBaseViewFrame)
         playerBaseView.backgroundColor = .black
         playerBaseView.clipsToBounds = true
-        playerBaseView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handleImageViewTap)))
+        playerBaseView.addGestureRecognizer(UITapGestureRecognizer(target: self, action: #selector(handlePlayerViewTap)))
         view.addSubview(playerBaseView)
         
         playerView = WKYTPlayerView(frame: playerBaseView.frame)
@@ -106,22 +105,21 @@ class VideoDetailViewController: UIViewController {
         playerBaseView.addSubview(playerView)
         playerBaseView.addConstraints(for: playerView)
         
-        imageViewCloseButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
-        imageViewCloseButton.isHidden = true
-        imageViewCloseButton.tintColor = .white
-        imageViewCloseButton.setImage(UIImage(systemName: "xmark"), for: .normal)
-        imageViewCloseButton.addTarget(self, action: #selector(handleImageViewCloseButtonTap), for: .touchUpInside)
-        playerBaseView.addSubview(imageViewCloseButton)
+        playerCloseButton = UIButton(frame: CGRect(x: 0, y: 0, width: 40, height: 40))
+        playerCloseButton.isHidden = true
+        playerCloseButton.tintColor = .white
+        playerCloseButton.setImage(UIImage(systemName: "xmark"), for: .normal)
+        playerCloseButton.addTarget(self, action: #selector(handlePlayerCloseButtonTap), for: .touchUpInside)
+        playerBaseView.addSubview(playerCloseButton)
     }
     
     private func setupDescriptionView() {
         descriptionBaseView = UIView(frame: CGRect(x: 0, y: playerBaseView.frame.maxY, width: view.frame.width, height: view.frame.height - playerBaseView.frame.height))
         view.addSubview(descriptionBaseView)
         
-        descriptionView = VideoDetailDescriptionView()
+        let descriptionView = VideoDetailDescriptionView()
         descriptionBaseView.addSubview(descriptionView)
         descriptionBaseView.addConstraints(for: descriptionView)
-
     }
     
     private func setupBackgroundMaskingView() {
@@ -194,7 +192,7 @@ class VideoDetailViewController: UIViewController {
         }
     }
     
-    @objc private func handleImageViewTap() {
+    @objc private func handlePlayerViewTap() {
         guard screenMode == .small else { return }
         
         UIView.animate(withDuration: 0.2) {
@@ -205,33 +203,33 @@ class VideoDetailViewController: UIViewController {
         }
     }
     
-    @objc  private func handleImageViewCloseButtonTap() {
+    @objc  private func handlePlayerCloseButtonTap() {
         SceneDelegate.shared?.videoDetailWindow?.close()
     }
 
-    func updateImageViewSizeAndPosition(minimizationProgress: CGFloat) {
+    func updatePlayerBaseViewSizeAndPosition(minimizationProgress: CGFloat) {
         let destinationWidth = UIApplication.shared.screen.bounds.width * 0.51
         // 高さは16:9の割合の計算で求める(width * 0.5625)
         let destinationHeight = destinationWidth * 0.5625
-        // 画面の横幅から(余白, 縮小後のImageViewの横幅)を差し引く
+        // 画面の横幅から(余白, 縮小後のplayerBaseViewの横幅)を差し引く
         let destinationMinX = UIApplication.shared.screen.bounds.width - 8 - destinationWidth
         let tabBarHeight = (SceneDelegate.shared?.mainWindow?.rootViewController as? TabBarController)?.tabBar.frame.height ?? 0.0
-        // 画面の高さから(TabBarの高さ, 余白, 縮小後のImageViewの高さ)を差し引く
+        // 画面の高さから(TabBarの高さ, 余白, 縮小後のplayerBaseViewの高さ)を差し引く
         let destinationMinY = UIApplication.shared.screen.bounds.height - tabBarHeight - 8 - destinationHeight
 
-        let currentMinX = currentValue(initialValue: initialImageViewFrame.minX,
+        let currentMinX = currentValue(initialValue: initialPlayerBaseViewFrame.minX,
                                        destinationValue: destinationMinX,
                                        progress: minimizationProgress,
                                        isDestinationValueGreaterThanInitialValue: true)
-        let currentMinY = currentValue(initialValue: initialImageViewFrame.minY,
+        let currentMinY = currentValue(initialValue: initialPlayerBaseViewFrame.minY,
                                        destinationValue: destinationMinY,
                                        progress: minimizationProgress,
                                        isDestinationValueGreaterThanInitialValue: true)
-        let currentWidth = currentValue(initialValue: initialImageViewFrame.width,
+        let currentWidth = currentValue(initialValue: initialPlayerBaseViewFrame.width,
                                        destinationValue: destinationWidth,
                                        progress: minimizationProgress,
                                        isDestinationValueGreaterThanInitialValue: false)
-        let currentHeight = currentValue(initialValue: initialImageViewFrame.height,
+        let currentHeight = currentValue(initialValue: initialPlayerBaseViewFrame.height,
                                        destinationValue: destinationHeight,
                                        progress: minimizationProgress,
                                        isDestinationValueGreaterThanInitialValue: false)
